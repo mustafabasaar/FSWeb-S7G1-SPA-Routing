ffe0f5ae7581c2792436e3159fc76d9313f4d0a8
mustafabasar96 <mustafabasar96@hotmail.com>
2023-10-03 13:40:56 +0300

    proje tamamlandı

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Switch, Route } from "react-router-dom";
+import KaydedilenlerListesi from "./Filmler/KaydedilenlerListesi";
+import FilmListesi from "./Filmler/FilmListesi";
+import Film from "./Filmler/Film";
 
-import KaydedilenlerListesi from './Filmler/KaydedilenlerListesi';
-
-export default function App () {
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const FilmleriAl = () => {
       axios
-        .get('http://localhost:5001/api/filmler') // Burayı Postman'le çalışın
-        .then(response => {
+        .get("http://localhost:5001/api/filmler") // Burayı Postman'le çalışın
+        .then((response) => {
           // Bu kısmı log statementlarıyla çalışın
-          // ve burdan gelen response'u 'movieList' e aktarın
+          // ve burdan gelen response'u 'movieList' e aktarı
+          setMovieList(response.data);
         })
-        .catch(error => {
-          console.error('Sunucu Hatası', error);
+        .catch((error) => {
+          console.error("Sunucu Hatası", error);
         });
-    }
+    };
     FilmleriAl();
   }, []);
 
-  const KaydedilenlerListesineEkle = id => {
-    // Burası esnek. Aynı filmin birden fazla kez "saved" e eklenmesini engelleyin
+  const KaydedilenlerListesineEkle = (id) => {
+    const varMi = saved.find((film) => film.id === Number(id));
+    console.log("varMi", varMi);
+    if (!varMi) {
+      const kaydedilecekFilm = movieList.find((film) => film.id === Number(id));
+      const yeniSavedState = [kaydedilecekFilm, ...saved];
+      setSaved(yeniSavedState);
+    }
   };
 
   return (
     <div>
-      <KaydedilenlerListesi list={[ /* Burası esnek */]} />
-
-      <div>Bu Div'i kendi Routelarınızla değiştirin</div>
+      <KaydedilenlerListesi list={saved} />
+      <div>
+        <Switch>
+          <Route path="/filmler/:id">
+            <Film KaydedilenlerListesineEkle={KaydedilenlerListesineEkle} />
+          </Route>
+          <Route path="/">
+            <FilmListesi movies={movieList} />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function KaydedilenlerListesi(props) {
+  const { list } = props;
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+      {list.map((movie) => (
+        <Link key={movie.id} to={`/filmler/${movie.id}`}>
+          <span className="saved-movie">{movie.title}</span>
+        </Link>
       ))}
-      <div className="home-button">Anasayfa</div>
+      <Link to="/">
+        <div className="home-button">Anasayfa</div>
+      </Link>
     </div>
   );
 }
